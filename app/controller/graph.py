8610370034cd6631df5fe372716440d11351cd13
Felipe Signorini <felipeklerk@yahoo.com.br>
2018-11-03 18:25:06 -0300

    apidocs

--- app/controller/graph.py
+++ app/controller/graph.py
@@ -10,26 +10,27 @@ from app.libs.helpers.filterTransformation import FilterTransformation
 
 
 class GraphApp(Resource):
-    # @api {post} /graph/ Graph Bussiness
-    # @apiName GetGraph
-    # @apiGroup FGraph
-    #
-    # @apiParam(Param) {String} owner_id Owner ID
-    # @apiParam(Param) {String} type Graph type [Bussiness]
-    # @apiParam(Param) {String} _id Graph id, need to be create by server app
-    #
-    # @apiPermission JWT (Read | Write | Admin)
-    # @apiHeader (Auth) {String} Authorization JWT {Token}
-    #
-    # @apiError (Error) PermissionError Token don`t have permission
-    # @apiError (Error) Unauthorized Invalid Token
-    # @apiError (Error) NotFound List is empty
-    #
-    # @apiSuccessExample {json} Success-Response:
-    #          HTTP/1.1 201 OK
-    #          {
-    #          }
+    """
+    @api {post} /graph/ Graph Bussiness
+    @apiName GetGraph
+    @apiGroup Graph
 
+    @apiParam(Param) {String} owner_id Owner ID
+    @apiParam(Param) {String} type Graph type [Bussiness]
+    @apiParam(Param) {String} _id Graph id, need to be create by server app
+
+    @apiPermission JWT (Read | Write | Admin)
+    @apiHeader (Auth) {String} Authorization JWT {Token}
+
+    @apiError (Error) PermissionError Token don`t have permission
+    @apiError (Error) Unauthorized Invalid Token
+    @apiError (Error) NotFound List is empty
+
+    @apiSuccessExample {json} Success-Response:
+            HTTP/1.1 201 OK
+             {
+             }
+    """
     def post(self):
 
         valid = Validate().validate()
--- app/controller/home.py
+++ app/controller/home.py
@@ -5,17 +5,19 @@ from flask_restful import Resource
 
 
 class HomeApp(Resource):
-    # @api {get} / Ping
-    # @apiName GetPing
-    # @apiGroup Ping
+    """
+    @api {get} / Ping
+    @apiName GetPing
+    @apiGroup Ping
 
-    # @apiSuccessExample {json} Success-Response:
-    # HTTP/1.1 201 OK
-    # {
-    #    app: (String)
-    #    description: (String)
-    #    version: (String)
-    # }
+    @apiSuccessExample {json} Success-Response:
+    HTTP/1.1 201 OK
+    {
+       app: (String)
+       description: (String)
+       version: (String)
+    }
+    """
     def get(self):
         root_path = os.path.join(app.root_path, '..')
 
